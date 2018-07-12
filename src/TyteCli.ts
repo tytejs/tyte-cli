531200069708d02dd8f3d9196ab7d2d67bd49893
Norris Oduro <norrisjibril@gmail.com>
2018-07-12 11:01:11 +0000

    Add support for config specification in the Application

--- src/TyteCli.ts
+++ src/TyteCli.ts
@@ -1,73 +1,115 @@
-import CommandInterface from './Command/CommandInterface'
-import InputInterface from './IO/InputInterface'
-import OutputInterface from './IO/OutputInterface'
-import Input from './IO/Input'
-import Output from './IO/Output'
-import * as commander from 'commander'
-import * as process from 'process'
-import * as color from 'colors' //use later for coloured output
-import CommandOptionInterface from './Command/CommandOptionInterface'
+import CommandInterface from "./Command/CommandInterface";
+import InputInterface from "./IO/InputInterface";
+import OutputInterface from "./IO/OutputInterface";
+import Input from "./IO/Input";
+import Output from "./IO/Output";
+import * as commander from "commander";
+import * as process from "process";
+import * as color from "colors"; //use later for coloured output
+import CommandOptionInterface from "./Command/CommandOptionInterface";
 
 class HelpCommand extends CommandInterface {
-  readonly name = 'help'
-  readonly description = 'Display help for a command'
-  _options: Array<CommandOptionInterface>
+  readonly name = "help";
+  readonly description = "Display help for a command";
+  _options: Array<CommandOptionInterface>;
   set options(options) {
-    this._options = options
+    this._options = options;
   }
   get options(): Array<CommandOptionInterface> {
     return [
       {
-        option: 'command',
-        description: 'The command'
+        option: "command",
+        description: "The command"
       }
-    ]
+    ];
   }
   execute(input: InputInterface, output: OutputInterface): void {
-    output.write('help [command]')
+    output.write("help [command]");
   }
 }
+
 export default class TyteCli {
-  private version: string = '1.0.0'
-  private commands: Array<CommandInterface> = new Array<CommandInterface>()
-  private input: InputInterface
-  private output: OutputInterface
+  private _version: string = "1.0.0";
+  private _versionCmd: string = "-v, --version";
+  private commands: Array<CommandInterface> = new Array<CommandInterface>();
+  private input: InputInterface;
+  private output: OutputInterface;
+  private appCommand: commander.CommanderStatic;
+  private _description: string = "Tyte framework cli component";
+  private _usage: string = "[options] [arguments]";
 
   constructor(
     input: InputInterface = new Input(),
     output: OutputInterface = new Output()
   ) {
-    this.input = input
-    this.output = output
-    commander
-      .version(this.version, '-v, --version')
-      .description('Tyte framework cli component')
-      .usage('[options] [arguments]')
-    this.commands.push(new HelpCommand())
+    this.input = input;
+    this.output = output;
+    this.appCommand = commander;
+  }
+
+  set version(version: string) {
+    this._version = version;
+  }
+
+  get version(): string {
+    return this._version;
+  }
+  set versionCmd(versionCmd: string) {
+    this._versionCmd = versionCmd;
+  }
+
+  get versionCmd(): string {
+    return this._versionCmd;
+  }
+
+  set description(description: string) {
+    this._description = description;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  set usage(usage: string) {
+    this._usage = usage;
+  }
+
+  get usage(): string {
+    return this._usage;
   }
 
   addCommand(command: CommandInterface): void {
-    this.commands.push(command)
+    this.commands.push(command);
+  }
+
+  private setUp(): void {
+    this.appCommand
+      .version(this.version, this._versionCmd)
+      .description(this.description)
+      .usage(this._usage);
   }
 
   run(): void {
+    //run the app config setup
+    this.setUp();
+    
     this.commands.forEach(cmd => {
-      let _command: commander.Command
-      _command = commander.command(cmd.name).description(cmd.description)
+      let _command: commander.Command;
+      _command = commander.command(cmd.name).description(cmd.description);
       cmd.options.forEach(option => {
-        _command.option(option.option, option.description)
-      })
-      let _this = this
+        _command.option(option.option, option.description);
+      });
+      let _this = this;
       _command.action(function() {
-        let args = Array.from(arguments).slice() //deep copy args
-        args.pop()
-        _this.input.setValues(args)
-        cmd.execute(_this.input, _this.output)
-      })
-    })
-    commander.parse(process.argv)
+        let args = Array.from(arguments).slice(); //deep copy args
+        args.pop();
+        _this.input.setValues(args);
+        cmd.execute(_this.input, _this.output);
+      });
+    });
+    commander.parse(process.argv);
     if (!process.argv.slice(2).length) {
-      commander.outputHelp()
+      commander.outputHelp();
     }
   }
 }
